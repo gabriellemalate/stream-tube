bdb7ba06966ca9607642eb07488385cd53d66bfa
Gabrielle Malate <gabriellemalate@gmail.com>
2019-02-18 10:04:05 -0800

    Async func to fetch select videos

--- streamtube-client/src/pages/Landing/Landing.js
+++ streamtube-client/src/pages/Landing/Landing.js
@@ -1,14 +1,19 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import "./Landing.scss"
 import axios from "axios";
 import VideoSection from "../../components/VideoSection/VideoSection";
+
+const apiUrl = "http://localhost:8080";
 
 function LandingPage() {
     const [videoData, setVideoData] = useState([]);
     const [selectedVideo, setSelectedVideo] = useState(null);
     const [loading, setLoading] = useState(true);
+    const [duration, setDuration] = useState(0);
+    const navigate = useNavigate();
     const { videoId } = useParams();
+
 
     const fetchData = async () => {
         try {
@@ -33,6 +38,30 @@ function LandingPage() {
     useEffect(() => {
         fetchData();
     }, [videoId]);
+
+    const fetchSelectedVideoDetails = async () => {
+        if (selectedVideo) {
+            try {
+                const response = await axios.get(`${apiUrl}/videos/${selectedVideo.id}`);
+
+                if (response.data) {
+                    const { duration } = response.data;
+                    setDuration(duration);
+                    setSelectedVideo(response.data);
+                } else {
+                    console.error("No details found for the selected video.");
+                }
+            } catch (error) {
+                console.error("Error fetching selected video details:", error);
+            }
+        }
+    };
+
+    useEffect(() => {
+        if (selectedVideo && !selectedVideo.comments) {
+            fetchSelectedVideoDetails();
+        }
+    }, [selectedVideo]);
 
     return (
         <>

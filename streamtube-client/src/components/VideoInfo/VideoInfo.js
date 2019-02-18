888557cb295876a9a6980d949e45f717a3748c26
Gabrielle Malate <gabriellemalate@gmail.com>
2019-02-18 10:04:05 -0800

    Pull data from API

--- streamtube-client/src/components/VideoInfo/VideoInfo.js
+++ streamtube-client/src/components/VideoInfo/VideoInfo.js
@@ -3,19 +3,26 @@ import "./VideoInfo.scss"
 import Eye from "../../assets/images/icons/views.svg";
 import Heart from "../../assets/images/icons/likes.svg";
 
-function VideoInfo() {
+function VideoInfo({ videoData }) {
+    if (!videoData) {
+        return null;
+    }
+
+    const { title, channel, timestamp, views, likes, description } = videoData;
+
+    
 
     return (
         <section className='video-info'>
             <div className='video-info__eq'>
                 <h1 className='video-info__head'>
-
+                    {title}
                 </h1>
 
                 <article className='video-info__legend'>
                     <div className='video-info__legend-left'>
                         <h3 className='video-info__legend-left-author'>
-
+                        {`By ${channel}`}
                         </h3>
                         <p className='video-info__legend-left-date'>
 
@@ -26,20 +33,20 @@ function VideoInfo() {
                         <p className='video-info__legend-right-views'>
                             <img className='video-info__legend-right-views-img' alt="Views Icon" src={Eye} />
                             <span className='video-info__legend-right-views-count'>
-                            
+                            {views}
                             </span>
                         </p>
                         <p className='video-info__legend-right-likes'>
                             <img className='video-info__legend-right-likes-img' alt="Likes Icon" src={Heart} />
                             <span className='video-info__legend-right-likes-count'>
-                                
+                                {likes}
                             </span>
                         </p>
                     </div>
                 </article>
 
                 <p className='video-info__paragraph'> 
-                
+                {description}
                 </p>
             </div>
         </section>

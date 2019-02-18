ea6fe91feccbbae67b1dc8174020c44ee5f350ad
Gabrielle Malate <gabriellemalate@gmail.com>
2019-02-18 10:04:05 -0800

    Initial page imports

---
+++ streamtube-client/src/pages/Upload/Upload.js
@@ -0,0 +1,18 @@
+import "./Upload.scss"
+import React from "react";
+import axios from "axios";
+import publish from "../../assets/images/icons/publish.svg";
+import preview from "../../assets/images/Upload-video-preview.jpg"
+
+
+function UploadPage() {
+
+    return (
+        <>
+            <main className="upload">
+            </main>
+        </>
+    );
+}
+
+export default UploadPage;

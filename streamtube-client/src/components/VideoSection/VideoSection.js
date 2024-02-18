c97da8206e0c946cd2eec4852923fda8989155db
Gabrielle Malate <gabriellemalate@gmail.com>
2024-02-18 10:54:37 -0800

    Initiated files for planned components

---
+++ streamtube-client/src/components/VideoSection/VideoSection.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./VideoSection.scss"
+
+function VideoSection() {
+
+    return (
+        <section className='video'>
+            <div className='video-eq'>
+                
+            </div>
+
+        </section>
+    );
+}
+
+export default VideoSection;

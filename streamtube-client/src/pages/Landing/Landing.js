34725cdccf1e7085712cc0d003077e080193d1c1
Gabrielle Malate <gabriellemalate@gmail.com>
2024-02-18 11:05:15 -0800

    Fetch video data from API

--- streamtube-client/src/pages/Landing/Landing.js
+++ streamtube-client/src/pages/Landing/Landing.js
@@ -1,13 +1,44 @@
 import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import "./Landing.scss"
 import axios from "axios";
+import VideoSection from "../../components/VideoSection/VideoSection";
 
 function LandingPage() {
+    const [videoData, setVideoData] = useState([]);
+    const [selectedVideo, setSelectedVideo] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const { videoId } = useParams();
+
+    const fetchData = async () => {
+        try {
+            const response = await axios.get(`${apiUrl}/videos`);
+            if (response.data.length > 0) {
+                setVideoData(response.data);
+
+                const selected =
+                    videoId && response.data.find((video) => video.id === videoId);
+
+                setSelectedVideo(selected || response.data[0]);
+            } else {
+                console.error("No videos found in the response.");
+            }
+        } catch (error) {
+            console.error("Error fetching data:", error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, [videoId]);
 
     return (
         <>
-        <main className="main">
-        </main>
+            <main className="main">
+                <VideoSection selectedVideo={selectedVideo} duration={selectedVideo ? selectedVideo.duration : 0} />
+            </main>
         </>
     );
 }

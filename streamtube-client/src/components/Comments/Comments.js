bdb82b8f553a4511b64c665220004b83ea482542
Gabrielle Malate <gabriellemalate@gmail.com>
2024-02-22 07:26:18 -0800

    Better BEM

--- streamtube-client/src/components/Comments/Comments.js
+++ streamtube-client/src/components/Comments/Comments.js
@@ -1,8 +1,8 @@
 import React from "react";
-import "./CommentOld.scss"
+import "./Comments.scss"
 import Empty from "../../assets/images/empty-image.png";
 
-function CommentOld({ comments }) {
+function Comments({ comments }) {
     const { id, name, timestamp, comment } = comments;
 
     const formattedDate = new Date(timestamp).toLocaleDateString("en-US", {
@@ -12,19 +12,19 @@ function CommentOld({ comments }) {
     });
 
     return (
-        <article className='comments-old'>
-            <div key={id} className='comments-old__eq'>
-                <img className='comments-old__img' src={Empty} alt="Commenter"></img>
-                <div className="comments-old__text">
-                    <div className="comments-old__text-top">
-                        <h3 className="comments-old__text-top-name">{name}</h3>
-                        <p className="comments-old__text-top-date">{formattedDate}</p>
+        <article className='comments'>
+            <div key={id} className='comments__eq'>
+                <img className='comments__img' src={Empty} alt="Commenter"></img>
+                <div className="comments__text">
+                    <div className="comments__text-top">
+                        <h3 className="comments__text-top-name">{name}</h3>
+                        <p className="comments__text-top-date">{formattedDate}</p>
                     </div>
-                    <p className="comments-old__text-comment">{comment}</p>
+                    <p className="comments__text-comment">{comment}</p>
                 </div>
             </div>
         </article>
     );
 }
 
-export default CommentOld;
+export default Comments;

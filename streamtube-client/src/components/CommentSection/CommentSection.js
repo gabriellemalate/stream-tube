9e3779d77aff05e82308f155e9ad8563e3061b72
Gabrielle Malate <gabriellemalate@gmail.com>
2024-02-22 07:34:40 -0800

    Handle per video post req of comments

--- streamtube-client/src/components/CommentSection/CommentSection.js
+++ streamtube-client/src/components/CommentSection/CommentSection.js
@@ -17,7 +17,7 @@ function Comments({ videoData, onAddComment }) {
                 <h4 className='comments__counter'>{`${comments.length} ${comments.length === 1 ? "Comment" : "Comments"
                     }`}</h4>
 
-
+                <CommentAdd onAddComment={onAddComment} videoId={videoData.id} />
 
                 {comments.map((comment) => (
                     <CommentOld key={comment.id} comments={comment} />

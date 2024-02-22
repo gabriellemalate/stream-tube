69015ca0b2a11760f0abad0e56e6e7438a96b316
Gabrielle Malate <gabriellemalate@gmail.com>
2024-02-22 07:22:20 -0800

    Component to add comment

---
+++ streamtube-client/src/components/CommentAdd/CommentAdd.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./CommentOld.scss"
+import Empty from "../../assets/images/empty-image.png";
+
+function CommentOld({ comments }) {
+    const { id, name, timestamp, comment } = comments;
+
+    
+
+    return (
+        <article className='comments-old'>
+            <div key={id} className='comments-old__eq'>
+                <img className='comments-old__img' src={Empty} alt="Commenter"></img>
+                <div className="comments-old__text">
+                    <div className="comments-old__text-top">
+                        <h3 className="comments-old__text-top-name">{name}</h3>
+                        <p className="comments-old__text-top-date">{formattedDate}</p>
+                    </div>
+                    <p className="comments-old__text-comment">{comment}</p>
+                </div>
+            </div>
+        </article>
+    );
+}
+
+export default CommentOld;

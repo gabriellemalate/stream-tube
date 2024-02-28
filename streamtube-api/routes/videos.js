9625100ae3ee669b70b538185d2059fb4c9599d9
Gabrielle Malate <gabriellemalate@gmail.com>
2024-02-28 01:24:19 -0800

    Initiate middleware

--- streamtube-api/routes/videos.js
+++ streamtube-api/routes/videos.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const fs = require("fs");
 const path = require('path');
 const { v4: uuidv4 } = require("uuid");
+const cors = require('cors'); // Import cors middleware
+
+router.use(cors());
 
 // read video data from json file
 function readVideosData() {

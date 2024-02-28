9f6f5097c037df53a717110f6fe51cf3ed2c8dc9
Gabrielle Malate <gabriellemalate@gmail.com>
2024-02-28 01:39:50 -0800

    Routed new page

--- streamtube-client/src/App.js
+++ streamtube-client/src/App.js
@@ -8,6 +8,7 @@ import UploadPage from "./pages/Upload/Upload";
 import SelectedVideo from "./pages/Landing/Landing";
 import Success from './components/Success/Success';
 import NotFound from './components/NotFound/Notfound';
+import UserPage from './pages/User/User';
 
 function App() {
 
@@ -19,6 +20,7 @@ function App() {
           <Route path='/' element={<LandingPage />} />
           <Route path='/videos/:videoId' element={<SelectedVideo />} />
           <Route path='/upload' element={<UploadPage />} />
+          <Route path='/sign' element={<UserPage/>} />
           <Route path='/success' element={<Success/>}/>
           <Route path='*' element={<NotFound />} />
         </Routes>
